ae6df535aa3523bdf27bdfb7288c96945f265f4d
skullbreakerr <96946235+skullbreakerr@users.noreply.github.com>
2023-06-22 14:48:45 +0530

    3 files modified

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 import './style.css';
 
-const scroll = new LocomotiveScroll({
-  el: document.querySelector('#main'),
-  smooth: true
-});
+// const scroll = new LocomotiveScroll({
+//   el: document.querySelector('#main'),
+//   smooth: true
+// });
 
-var tl = gsap.timeline()
-tl.to("#page1",{
-  y:"100vh",
-  scale:0.6,
-  duration:0
-})
-tl.to("#page1",{
-  y:"30vh",
-  duration:1,
-  delay:1
-})
+// var tl = gsap.timeline()
+// tl.to("#page1",{
+//   y:"100vh",
+//   scale:0.6,
+//   duration:0
+// })
+// tl.to("#page1",{
+//   y:"30vh",
+//   duration:1,
+//   delay:1
+// })
 
-tl.to("#page1",{
-  y:"0vh",
-  rotate:360,
-  scale:1,
-  duration:0.8
-})
+// tl.to("#page1",{
+//   y:"0vh",
+//   rotate:360,
+//   scale:1,
+//   duration:0.8
+// })
