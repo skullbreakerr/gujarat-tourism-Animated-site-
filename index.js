4f96f136594e7e81fa7e156777d7a0afa8299a60
skullbreakerr <96946235+skullbreakerr@users.noreply.github.com>
2023-06-22 17:26:27 +0530

    2 files modified

--- index.js
+++ index.js
@@ -1,25 +1,44 @@
 import './style.css';
 
-// const scroll = new LocomotiveScroll({
-//   el: document.querySelector('#main'),
-//   smooth: true
-// });
+const scroll = new LocomotiveScroll({
+  el: document.querySelector('#main'),
+  smooth: true
+});
 
-// var tl = gsap.timeline()
-// tl.to("#page1",{
-//   y:"100vh",
-//   scale:0.6,
-//   duration:0
-// })
-// tl.to("#page1",{
-//   y:"30vh",
-//   duration:1,
-//   delay:1
-// })
+var tl = gsap.timeline()
 
-// tl.to("#page1",{
-//   y:"0vh",
-//   rotate:360,
-//   scale:1,
-//   duration:0.8
-// })
+tl.to(["#page1","#page2"],{
+  y:"100vh",
+  scale:0.6,
+  duration:0
+})
+tl.to("#page1",{
+  y:"0vh",
+  scale:1,
+  duration:1
+})
+tl.to("#page2",{
+  y:"0vh",
+  scale:1,
+  duration:1.2
+})
+tl.to(["#page1","#page2"],{
+  y:"30vh",
+  scale:1.2,
+  duration:1,
+  delay:1
+})
+tl.to("#page1",{
+  y:"0vh",
+  rotate:360,
+  scale:1,
+  duration:1.8,
+  delay:1
+})
+tl.to("#page2",{
+  y:"0vh",
+  rotate:360,
+  scale:1,
+  duration:1.8,
+  delay:-1.7
+})
